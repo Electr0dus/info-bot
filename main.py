0509a99b292f1826fb35d4b91e6533785d7f4df9
Artyom <tiom.teomin@yandex.ru>
2024-11-17 21:49:58 +0300

    Скелет бота

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from aiogram import bot, dispatcher, types, executor
+from aiogram import Bot, Dispatcher, types, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import logging
+import config_bot
+
+bot = Bot(config_bot.TOKEN_API)
+dp = Dispatcher(bot=bot, storage=MemoryStorage)
+logging.basicConfig(level=logging.INFO, filemode='w', filename='bot.log',
+                    format='%(asctime)s | %(levelname)s | %(message)s')
+
 
 
 
 def main():
-    executor.start_polling()
+    logging.info('START BOT')
+    executor.start_polling(dispatcher=dp, skip_updates=True)
 
 if __name__ == '__main__':
     main()
